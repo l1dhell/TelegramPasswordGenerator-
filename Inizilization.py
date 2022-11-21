d0abba10d7ebb18dd276ff2e46d26707d8875af0
usersdss <alexmanukian@gmail.com>
2022-11-21 16:50:52 +0300

    Working producinline buttons. One button doesn't workt. waiting for updates

--- Inizilization.py
+++ Inizilization.py
@@ -2,9 +2,11 @@ import secrets
 import string
 from aiogram import Bot, Dispatcher,executor,types
 from config import TOKEN,ADMIN
-from keyboards import Keyboard
+from aiogram.types import InlineKeyboardButton,InlineKeyboardMarkup
+
 
 def GeneratePassword(pwd_length):
+    pwd_length = int(pwd_length)
     letters = string.ascii_letters
     digits = string.digits
     special_chars = string.punctuation
@@ -23,23 +25,35 @@ def GeneratePassword(pwd_length):
               break
     return pwd
 
+Keyboard = InlineKeyboardMarkup(resize_keyboard=True,one_time_keyboard=True)
+Button_DoGenerate = InlineKeyboardButton("Generate Password",callback_data="Generate Password")
+Button_SetValue = InlineKeyboardButton("Set password length",callback_data="Set your lenght")
+Keyboard.add(Button_DoGenerate)
+Keyboard.add(Button_SetValue)
+
 bot = Bot(token=TOKEN)
 dp = Dispatcher(bot)
 
-@dp.message_handler(commands=["start"])
-async def send_welcome(message: types.Message):
-    await message.answer("Hello",reply_markup=Keyboard)
 
-@dp.message_handler(commands=["GeneratePassword"],state=None)
-async def Generator(message:types.message):
-    await message.answer("Your password")
-    Password = GeneratePassword(12)
-    await message.answer(f":{Password}")
+
+@dp.callback_query_handler(text=["Generate Password","Set your lenght"])
+async def Generator(call:types.CallbackQuery):
+    if call.data == "Generate Password":
+        await call.message.answer(f"{GeneratePassword(12)}",reply_markup=Keyboard)
+    if call.data == "Set your lenght":
+        await call.message.answer("Send me a value")
+        LenghtPassword = call.data
+        await call.message.answer(f"{GeneratePassword(LenghtPassword)}")
+
 
 #Включение бота
 async def on_startup(_):
     await bot.send_message(ADMIN, "Bot is ready")
     print("Bot is working")
+
+@dp.message_handler(commands=["start"])
+async def send_welcome(message: types.Message):
+    await message.answer("Hello",reply_markup=Keyboard)
 
 if __name__ == '__main__':
   executor.start_polling(dp, skip_updates=True,on_startup=on_startup)
