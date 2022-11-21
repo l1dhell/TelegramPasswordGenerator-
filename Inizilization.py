c2354711ca081b2c1179e36ee31e7cacc3c7fd7e
usersdss <alexmanukian@gmail.com>
2022-11-21 14:47:23 +0300

    First working version

---
+++ Inizilization.py
@@ -0,0 +1,45 @@
+import secrets
+import string
+from aiogram import Bot, Dispatcher,executor,types
+from config import TOKEN,ADMIN
+from keyboards import Keyboard
+
+def GeneratePassword(pwd_length):
+    letters = string.ascii_letters
+    digits = string.digits
+    special_chars = string.punctuation
+
+    alphabet = letters + digits + special_chars
+
+    #pwd_length = 12
+
+    while True:
+      pwd = ''
+      for i in range(pwd_length):
+        pwd += ''.join(secrets.choice(alphabet))
+
+      if (any(char in special_chars for char in pwd) and
+          sum(char in digits for char in pwd)>=2):
+              break
+    return pwd
+
+bot = Bot(token=TOKEN)
+dp = Dispatcher(bot)
+
+@dp.message_handler(commands=["start"])
+async def send_welcome(message: types.Message):
+    await message.reply("Hello",reply_markup=Keyboard)
+
+@dp.message_handler(commands=["GeneratePassword"])
+async def Generator(message:types.message):
+    await message.reply("How long will the password be? \nSend me a number.")
+    Password = GeneratePassword(12)
+    await message.reply(f"Your password:{Password}")
+
+#Включение бота
+async def on_startup(_):
+    await bot.send_message(ADMIN, "Bot is ready")
+    print("Bot is working")
+
+if __name__ == '__main__':
+  executor.start_polling(dp, skip_updates=True,on_startup=on_startup)
---
+++ keyboards.py
@@ -0,0 +1,5 @@
+from aiogram.types import ReplyKeyboardRemove,ReplyKeyboardMarkup, KeyboardButton
+
+Keyboard = ReplyKeyboardMarkup(row_width=3,resize_keyboard=True,one_time_keyboard=True)
+Button_DoGenerate = KeyboardButton("Generate Password")
+Keyboard.row(Button_DoGenerate)
